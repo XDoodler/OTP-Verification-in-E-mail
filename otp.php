1dade163931cd0b8520655b9063c544d444b1c50
Soham Chakrabarti <soham17041998@gmail.com>
2018-02-23 09:32:14 +0530

    Update otp.php

--- otp.php
+++ otp.php
@@ -11,27 +11,27 @@ if($_SERVER['REQUEST_METHOD']=='POST')
         
         $pos=strpos($email,"@");
        
-        $cookieid=substr($email,0,$pos);
+        $cookieid=substr($email,0,$pos); //store the email userid
       
-        $otp=rand(1000,9999);
+        $otp=rand(1000,9999); //generate an OTP
        
 
-       if(!isset($_COOKIE["uid"])){
+       if(!isset($_COOKIE["uid"])){ //if cookie is set, then...
            
-           session_id("otp");session_start();
+           session_id("otp");session_start(); //start sessiom=n
 
-        setcookie("uid",$cookieid);
-        setcookie("email",$email);
-        setcookie($cookieid,$otp, time() +(3600*30));
+        setcookie("uid",$cookieid); // set cookie uid to store Userid from email
+        setcookie("email",$email);  // set cookie ei=mail to store email
+        setcookie($cookieid,$otp, time() +(3600*30)); //set otp cookie attached to userid
          header('Location: otp.html');
           }
 
         if(isset($_COOKIE["uid"])){
-         session_id("otp");session_start(); 
+         session_id("otp");session_start(); //session starts
           $to = $email;
-        $subject = "TOURLANCERS";
+        $subject = "SUBJECT";
         $txt = "VERIFICATION OTP: ".$_COOKIE[$cookieid]."   Valid for 10 minutes." ;
-        $headers = "From: info@tourlancers.com" ;
+        $headers = "From: otp@yourdomain.com" ;
         mail($to,$subject,$txt,$headers);
         header('Location: check.html');
           }
