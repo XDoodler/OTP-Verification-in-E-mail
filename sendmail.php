a7ecfc1c9b607d75231afbad73a13feec123050e
Soham Chakrabarti <soham17041998@gmail.com>
2018-03-15 10:16:29 +0530

    Add files via upload

--- sendmail.php
+++ sendmail.php
@@ -2,9 +2,9 @@
     session_id("otp");session_start();  
         $password=$_COOKIE["uid"];
         $to = $_COOKIE["email"];
-        $subject = "TOURLANCERS";
-        $txt = "VERIFICATION OTP: ".$_COOKIE[$password]." Valid for 10 minutes." ;
-        $headers = "From: info@tourlancers.com" ;
+        $subject = "OTP Verification";
+        $txt = "VERIFICATION OTP: ".$_COOKIE[$password]." Kindly Enter in the OTP Box" ;
+        $headers = "From: otp@your_domain.com" ;
         mail($to,$subject,$txt,$headers);
         header('Location: check.html');
 ?>
